7053eb32aace15b28b05baf81c7af3bf317f7143
吕永文 <11114396@bogon.vivo.xyz>
2020-12-25 19:57:25 +0800

    提交第一个例子

---
+++ spring-boot-example/src/main/java/org/springframework/boot/example/HelloApplication.java
@@ -0,0 +1,20 @@
+package org.springframework.boot.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <b>First Application</b>
+ *
+ * @author 11114396 lvyongwen
+ * @date 2020-12-25 17:16
+ * @since 1.0
+ */
+@SpringBootApplication
+public class HelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApplication.class,args);
+	}
+
+}
